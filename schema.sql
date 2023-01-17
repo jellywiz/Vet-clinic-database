066a1c048b7194c437054cea204231804e99e093
jellywiz <muzaffer.dler@gmail.com>
2023-01-17 22:54:36 +0300

    update the schema

--- schema.sql
+++ schema.sql
@@ -11,3 +11,23 @@ CREATE TABLE animals (
 
 /* update the table animals by using alter */
 Alter Table animals ADD COLUMN species VARCHAR(100);
+
+
+-- query multiple tables pr3
+CREATE TABLE owners (
+	id SERIAL PRIMARY KEY,
+  full_name VARCHAR(100) NOT NULL,
+  age INTEGER
+)
+
+CREATE TABLE species (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL
+)
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INTEGER;
+ALTER TABLE animals ADD COLUMN owner_id INTEGER;
+
+
